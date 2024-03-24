45c0728706b167ec63b89b09070191dc0eef7816
Jude Miracle <judemiraco0@gmail.com>
2024-03-24 09:57:02 +0100

    final comiit

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+const INFURA_RPC_URL = process.env.INFURA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: QUICKNODE_RPC_URL,
+      url: `https://sepolia.infura.io/v3/${INFURA_RPC_URL}`,
       accounts: [PRIVATE_KEY],
     },
   },
